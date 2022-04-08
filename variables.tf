69b206748fe2b55a00c09560e46d1a72ec371efa
tfdc-student <101262372+tfdc-student@users.noreply.github.com>
2022-04-08 09:59:39 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "nd_username" {
+    type = string
+}
+
+variable "nd_password" {
+    type = string
+}
+
+variable "nd_url" {
+    type = string
+}
+
+variable "platform" {
+    type = string
+}
